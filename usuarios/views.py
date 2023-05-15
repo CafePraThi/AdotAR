4ba22d9ed6d08adcd0930171fd76b0aafdd0986c
CafePraThi <cafeprathi@gmail.com>
2023-05-14 22:42:26 -0300

    cadastro users

---
+++ usuarios/views.py
@@ -0,0 +1,38 @@
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos.')
+            return render(request, 'cadastro.html')
+
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas digitadas não coincidem')
+            return render (request, 'cadastro.html')
+
+        try:
+            user = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha,
+            )
+            messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com Sucesso')
+            return render(request, 'cadastro.html')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro no cadastro, tente novamente mais tarde!')
+            return render(request, 'cadastro.html')
+
+
+        return HttpResponse('teste')
