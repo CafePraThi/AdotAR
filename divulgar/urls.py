c8451cb48b47ccd53f8e2df8c98783603e534e80
CafePraThi <cafeprathi@gmail.com>
2023-05-17 15:55:11 -0300

    remover pet

--- divulgar/urls.py
+++ divulgar/urls.py
@@ -6,4 +6,7 @@ app_name = 'divulgar'
 
 urlpatterns = [
     path('novo_pet/', views.novo_pet, name="novo_pet"),
+    path('seus_pets/', views.seus_pets, name="seus_pets"),
+    path('remover_pet/<int:id>', views.remover_pet, name="remover_pet")
+
 ]
--- divulgar/views.py
+++ divulgar/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.messages import constants
+from django.shortcuts import redirect, render
 
 from .models import Pet, Raca, Tag
 
@@ -41,47 +43,6 @@ def novo_pet(request):
         for tag_id in tags:
             tag = Tag.objects.get(id=tag_id)
             pet.tags.add(tag)
-```python
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Pet, Tag, Raca
-from django.contrib import messages
-from django.contrib.messages import constants
-
-@login_required
-def novo_pet(request):
-    if request.method == "GET":
-        tags = Tag.objects.all()
-        racas = Raca.objects.all()
-        return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
-    elif request.method == "POST":
-        foto = request.FILES.get('foto')
-        nome = request.POST.get('nome')
-        descricao = request.POST.get('descricao')
-        estado = request.POST.get('estado')
-        cidade = request.POST.get('cidade')
-        telefone = request.POST.get('telefone')
-        tags = request.POST.getlist('tags')
-        raca = request.POST.get('raca')
-
-        #TODO: Validar dados
-
-        pet = Pet(
-            usuario=request.user,
-            foto=foto,
-            nome=nome,
-            descricao=descricao,
-            estado=estado,
-            cidade=cidade,
-            telefone=telefone,
-            raca_id=raca,
-        )
-
-        pet.save()
-        
-        for tag_id in tags:
-            tag = Tag.objects.get(id=tag_id)
-            pet.tags.add(tag)
 
         pet.save()
         tags = Tag.objects.all()
@@ -89,3 +50,25 @@ def novo_pet(request):
         messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
         return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
 
+
+@login_required
+def seus_pets(request):
+    if request.method == "GET":
+        pets = Pet.objects.filter(usuario=request.user)
+        return render(request, 'seus_pets.html', {'pets': pets})
+
+
+@login_required
+def remover_pet(request, id):
+    pet = Pet.objects.get(id=id)
+
+    if not pet.usuario == request.user:
+        messages.add_message(request, constants.ERROR,
+                             "Não foi possivel excluir o pet selecionado.")
+        return redirect('/divulgar/seus_pets')
+
+    pet.delete()
+
+    messages.add_message(request, constants.SUCCESS,
+                         "Pet removido com sucesso.")
+    return redirect('/divulgar/seus_pets')
